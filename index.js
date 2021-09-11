96939082cd8f434f23af3b94d3640f81bdc23044
Ivan Boban <ivan.boban911@gmail.com>
2021-09-11 20:36:21 +0200

    Added temporary persistance for messages

--- index.js
+++ index.js
@@ -6,28 +6,27 @@ const server = http.createServer(app);
 
 const io = new Server(server);
 
+let messages = [];
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
 io.on("connect", (socket) => {
+  socket.on("connection", () => {
+    socket.broadcast.emit("New user", "New user joined");
+    socket.emit("previous messages", messages);
+  });
+
   socket.on("disconnect", () => {
-    emitMessage("User disconnected");
+    socket.emit("chat message", "user left");
   });
 
   socket.on("chat message", (msg) => {
-    emitMessage(msg);
+    io.emit("chat message", msg);
+    messages.push(msg);
   });
-});
-
-io.on("connection", (socket) => {
-  socket.broadcast.emit("New user joined");
 });
 
 server.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
-
-function emitMessage(msg) {
-  io.emit("chat message", msg);
-}
